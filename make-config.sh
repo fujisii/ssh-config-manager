0601491338ebfc665015042ee6c1e4367f2b1a0c
fujisii <51264835+fujisii@users.noreply.github.com>
2022-06-23 12:21:59 +0900

    update: 隠しファイルを除外対象に追加

--- make-config.sh
+++ make-config.sh
@@ -21,19 +21,53 @@ if [ -n "$2" ]; then
 fi
 
 # --------------------
+# 除外するファイルの設定
+# --------------------
+# 除外するファイルの設定
+array=()
+array+=(".gitkeep")
+
+# macOSの隠しファイルや一時ファイル等を除外する
+# 参考: https://github.com/github/gitignore/blob/main/Global/macOS.gitignore
+array+=(".DS_Store")
+array+=(".AppleDouble")
+array+=(".LSOverride")
+array+=("Icon")
+array+=("._*")
+array+=(".DocumentRevisions-V100")
+array+=(".fseventsd")
+array+=(".Spotlight-V100")
+array+=(".TemporaryItems")
+array+=(".Trashes")
+array+=(".VolumeIcon.icns")
+array+=(".com.apple.timemachine.donotpresent")
+array+=(".AppleDB")
+array+=(".AppleDesktop")
+array+=("\"Network Trash Folder\"")
+array+=("\"Temporary Items\"")
+array+=(".apdisk")
+
+# ファイル名に特定の文字列を含むファイルを除外する
+if "${is_dev}"; then
+    array+=("*production*")
+    array+=("*tool*")
+fi
+
+conditions=""
+for v in "${array[@]}"
+do
+    conditions+=" -not -name ${v}"
+done
+
+# --------------------
 # configファイルの作成
 # --------------------
 
 # リポジトリディレクトリの絶対パス
 cd `dirname $0`
 repository=`pwd`
-# ファイル名に特定の文字列を含むファイルを除外
-conditions="-not -name '.gitkeep' -not -name '.DS_Store'"
-if "${is_dev}"; then
-    conditions+=" -not -name '*production*' -not -name '*tool*'"
-fi
-files=`eval find config.d -type f $conditions | sort -n`
 
+files=`eval find config.d -type f $conditions | sort -n`
 rm -f config
 
 for file in ${files}
